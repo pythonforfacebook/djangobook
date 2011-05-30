7f7fb448255d8dc39e292a9fd57f6821d87d895d
Aidan Lister <aidan@php.net>
2011-05-31 00:07:07 +0200

    Whitespace changes

--- djangobook/models.py
+++ djangobook/models.py
@@ -10,26 +10,26 @@ class Facebook:
     Facebook instances hold information on the current user and
     the page he/she is accessing the application from, as well as
     the signed request that information is derived from.
-    
+
     Properties:
     user -- A User instance.
     page -- A FacebookPage instance.
     signed_request -- A string describing the raw signed request.
     """
-    
+
     user, page, signed_request = [None] * 3
 
 class FacebookPage:
     """
     FacebookPage instances represent Facebook Pages.
-    
+
     Properties:
     id -- An integer describing the id of the page.
     is_admin -- A boolean describing whether or not the current user is an administrator of the page.
     is_liked -- A boolean describing whether or not the current user likes the page.
     url -- A string describing the URL to the page.
     """
-    
+
     def __init__(self, id, is_admin, is_liked):
         self.id = id
         self.is_admin = is_admin
@@ -39,7 +39,7 @@ class FacebookPage:
 class User(models.Model):
     """
     Instances of the User class represent Facebook users who have authorized the application.
-    
+
     Properties:
     facebook_id -- An integer describing the user's Facebook ID.
     first_name -- A string describing the user's first name.
@@ -62,7 +62,7 @@ class User(models.Model):
     created_at - A datetime object describing when the user was registered.
     oauth_token -- An OAuth Token object.
     """
-    
+
     facebook_id = models.BigIntegerField()
     facebook_username = models.CharField(max_length=255, blank=True, null=True)
     first_name = models.CharField(max_length=255, blank=True, null=True)
@@ -83,26 +83,26 @@ class User(models.Model):
     oauth_token = models.OneToOneField('OAuthToken')
     created_at = models.DateTimeField(auto_now_add=True)
     last_seen_at = models.DateTimeField(auto_now_add=True)
-    
+
     @property
     def full_name(self):
         if self.first_name and self.last_name:
             return "%s %s" % (self.first_name, self.last_name)
-        
+
     @property
     def picture(self):
         connection = HTTPConnection('graph.facebook.com')
         connection.request('GET', '%s/picture' % self.facebook_id)
         response = connection.getresponse()
         return response.getheader('Location')
-        
+
     def synchronize(self):
         """Synchronize the user with Facebook's Graph API."""
         if self.oauth_token.expired:
             raise ValueError('Signed request expired.')
-        
+
         profile = get_facebook_profile(self.oauth_token.token)
-        
+
         self.facebook_id = profile.get('id')
         self.facebook_username = profile.get('username')
         self.first_name = profile.get('first_name')
@@ -119,9 +119,9 @@ class User(models.Model):
         self.locale = profile.get('locale')
         self.verified = profile.get('verified')
         self.birthday = datetime.strptime(profile['birthday'], '%m/%d/%Y') if profile.has_key('birthday') else None
-        
+
         self.save()
-    
+
     @property
     def graph(self):
         """Return a GraphAPI instance with the user's access token."""
@@ -130,15 +130,15 @@ class User(models.Model):
             return GraphAPI(self.oauth_token.token)
         except ImportError:
             pass
-            
+
         try:
             from facebook import GraphAPI
             return GraphAPI(self.oauth_token.token)
         except ImportError:
             pass
-        
+
         raise ImportError('Neither Facepy nor Facebook-SDK could be imported')
-        
+
     def __unicode__(self):
         if self.full_name:
             return self.full_name
@@ -150,12 +150,12 @@ class User(models.Model):
 class OAuthToken(models.Model):
     """
     Instances of the OAuthToken class are credentials used to query the Facebook API on behalf of a user.
-    
+
     token -- A string describing the OAuth token itself.
     issued_at -- A datetime object describing when the token was issued.
     expires_at -- A datetime object describing when the token expires (or None if it doesn't)
     """
-    
+
     token = models.CharField(max_length=255)
     issued_at = models.DateTimeField()
     expires_at = models.DateTimeField(null=True, blank=True)
@@ -163,7 +163,7 @@ class OAuthToken(models.Model):
     @property
     def expired(self):
         return self.expires_at < datetime.now() if self.expires_at else False
-        
+
     class Meta:
         verbose_name = 'OAuth token'
         verbose_name_plural = 'OAuth tokens'
